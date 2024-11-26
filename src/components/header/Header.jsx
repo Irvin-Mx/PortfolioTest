063f7a0ff635afad13d7b7aa3ea16e3bb8d68c58
Irvin Martinez <irvin0329@outlook.com>
2024-11-26 09:23:46 -0600

    Added main header and footer component folders and started header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,26 @@
+//Header component
+const Header = () => {
+    return (
+        <header>
+            <h2>Irvin Martinez</h2>
+            <nav>
+                <ul>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+            <section>
+                <div>
+                    <h4>Hi, my name is</h4>
+                    <h2>Irvin Martinez</h2>
+                    <h1>I build websites</h1>
+                </div>
+                <p>I am a JavaScript FullStack Web developer, and have worked with many diferent teams in agile environments, Worked with ReactJs, VueJs, Postgres Go specialized in building websites with t</p>
+            </section>
+
+        </header>
+    )
+}
+
+//Export component
+export default Header
