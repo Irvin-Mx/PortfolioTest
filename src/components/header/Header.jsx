dd9fd2dd612ae861e3e0d82b1b511b3265b85272
Irvin Martinez <irvin0329@outlook.com>
2024-11-27 21:37:01 -0600

    Integrated Tailwind and added styles to  Header.jsx component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,26 +1,37 @@
 //Header component
 const Header = () => {
-    return (
-        <header>
-            <h2>Irvin Martinez</h2>
-            <nav>
-                <ul>
-                    <li>Projects</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-            <section>
-                <div>
-                    <h4>Hi, my name is</h4>
-                    <h2>Irvin Martinez</h2>
-                    <h1>I build websites</h1>
-                </div>
-                <p>I am a JavaScript FullStack Web developer, and have worked with many diferent teams in agile environments, Worked with ReactJs, VueJs, Postgres Go specialized in building websites with t</p>
-            </section>
+  return (
+    <header className="flex flex-col h-screen">
+      <h2 className="text-center items-center p-4 font-mono text-lg">
+        Victor Martinez
+      </h2>
 
-        </header>
-    )
-}
+      <nav className="flex justify-center h-10">
+        <ul className="flex w-1/2 justify-evenly">
+          <li className="">Projects</li>
+          <li className="">Contact</li>
+        </ul>
+      </nav>
+
+      <section className="h-5/6">
+        <div className="h-5/6">
+          <h4 className=" text-sm h-1/5 flex items-end">
+            Hi, my name is
+          </h4>
+          <h2 className=" text-6xl animate-animate text-red-700">
+            Victor Martinez
+          </h2>
+          <h1 className="text-4xl">
+            Your nerdy friend that loves coding creative solutions for amazing projects.
+          </h1>
+        </div>
+        <p className=" bg-red-700">
+          Full Stack Web Developer
+        </p>
+      </section>
+    </header>
+  );
+};
 
 //Export component
-export default Header
+export default Header;
